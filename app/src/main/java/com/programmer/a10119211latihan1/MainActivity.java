20aba621463816119a65b9d0927927bbe23930ba
bdrsmsdn <badrasam7@gmail.com>
2022-04-19 21:06:27 +0700

    first commit

---
+++ app/src/main/java/com/programmer/a10119211latihan1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.programmer.a10119211latihan1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//NIM                   : 10119211
+//NAMA                  : BADRA SAMSUDIN RAMDAN NUGRAHA
+//TANGGAL PENGERJAAN    : 19 APRIL,2022
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
